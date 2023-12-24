b7292c6c1cfd8f8dbd93b8d4d0bb1e14be180a02
Vallydei <obedkova.v@rambler.ru>
2023-12-24 20:10:26 +0300

    updated 1

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../server/dist'
+    outDir: './dist'
 },
 base: '/',
 });
